8e4e289d7fed60bd9e716a4cb20d3768036988c4
liushuangMac <502277775@qq.com>
2023-10-29 22:50:52 +0800

    domain

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '77dd58a28aab461889d54bef71fc8b10',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://or4444.notion.site/77dd58a28aab461889d54bef71fc8b10?pvs=4',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
